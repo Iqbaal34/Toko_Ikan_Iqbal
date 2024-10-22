bd631b9fc05653843e233bf60e7098a0d828fd47
Iqbaal34 <141095635+Iqbaal34@users.noreply.github.com>
2024-10-22 10:21:34 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server='localhost' ;
-$username='root' ;
-$password='' ;
-$database='iqbal ocean fresh' ;
-
-$koneksi=mysqli_connect($server, $username, $password, $database) or
-die('Hidupkan Server') ;
-?>
